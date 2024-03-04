69928d7982fe68e04cbb2967f28f98f7e4b21ae9
Carlos Florêncio <carlosmflorencio@gmail.com>
2024-03-04 21:50:54 +0000

    add * to visual selection

--- init.lua
+++ init.lua
@@ -425,3 +425,19 @@ vim.cmd([[au InsertLeave * set nu rnu]])
 vim.keymap.set({ "n", "x", "i" }, "<C-n>", function()
 	require("vscode-multi-cursor").addSelectionToNextFindMatch()
 end)
+
+-- makes * and # work on visual mode too.
+vim.api.nvim_exec(
+	[[
+  function! g:VSetSearch(cmdtype)
+    let temp = @s
+    norm! gv"sy
+    let @/ = '\V' . substitute(escape(@s, a:cmdtype.'\'), '\n', '\\n', 'g')
+    let @s = temp
+  endfunction
+
+  xnoremap * :<C-u>call g:VSetSearch('/')<CR>/<C-R>=@/<CR><CR>
+  xnoremap # :<C-u>call g:VSetSearch('?')<CR>?<C-R>=@/<CR><CR>
+]],
+	false
+)
